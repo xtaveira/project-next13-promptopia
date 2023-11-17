ed08d944ed4e45c4134a3657a8f2b488444c2d66
Davi Taveira <davimtaveira@gmail.com>
2023-11-17 20:29:01 -0300

    fix import type

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,7 +6,7 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
-import Profile from '@components/profile'
+import Profile from '@components/Profile'
 
 const MyProfile = () => {
 
